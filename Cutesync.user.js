e91b8ab3e639748cf08cfd7e0d0fd6d72c0648e1
Kinu <mail@kinuch.in>
2013-11-15 19:42:27 +0000

    1.0.1
    Added syncing on auto update
    Fixed undefined trips

--- Cutesync.user.js
+++ Cutesync.user.js
@@ -1,67 +1,92 @@
 // ==UserScript==
-// @name	Cutesync 
+// @name	Cutesync
+// @description	Tiny, read-only namesync for 4chan's mobile layout. Does not require 4chan X.
 // @namespace	im.a.gay.cat
 // @author	team !kittensORw
 // @credits	Milky and his Namesync
 // @include	*4chan.org/b/*
 // @include	*4chan.org/soc/*
-// @version	1.0.0
+// @version	1.0.1
 // ==/UserScript==
 
-function init(){
+function initCutesync(){
 
-	var d, db, $, $$;
+        var d, db, $, $$;
 
-	d = document;
-	db = document.body;
+        d = document;
+        db = document.body;
 
-	$ = function(s, p){
-			if(!p || p == null){
-				p = db;
-			}
-			return p.querySelector(s);
-		}	
-		
-	$$ = function(s, p){
-			if(!p || p == null){
-				p = db;
-			}
-			return p.querySelectorAll(s);
-		}		
-		
-	$.htm = function(s, v){
-			if(!v || v == null){
-				return s.innerHTML;
-			}else{
-				s.innerHTML = v;
-			}
-			return s;
-		}		
-		
-	$.get = function(t, b){
-			var x = new XMLHttpRequest();
-			x.overrideMimeType('application/json');
-			x.onreadystatechange = function () {
-				if (x.readyState == 4) {
-					syncData = JSON.parse(x.responseText);
-					for(var ii=0; ii < syncData.length; ii++){
-							var tgt = $('.name', $('#pc'+syncData[ii]['p'], $('#t'+t)));
-							var synced = $.htm(tgt, (syncData[ii]['n'] || 'Anonymous') + ' <span style="font-weight:normal;">' + syncData[ii]['t'] + '</span>');
+        $ = function(s, p){
+					if(!p || p == null){
+							p = db;
 					}
-					return x.responseText;
+					return p.querySelector(s);
+                }        
+                
+        $$ = function(s, p){
+					if(!p || p == null){
+							p = db;
+					}
+					return p.querySelectorAll(s);
+                }                
+                
+        $.htm = function(s, v){
+					if(!v || v == null){
+							return s.innerHTML;
+					}else{
+							s.innerHTML = v;
+					}
+					return s;
+                }                
+                
+        $.get = function(t, b){
+				var x = new XMLHttpRequest();
+				x.overrideMimeType('application/json');
+				x.onreadystatechange = function () {
+					if (x.readyState == 4) {
+						var syncData, tgt, synced, rTrip, rName;
+						syncData = JSON.parse(x.responseText);
+						for(var ii=0; ii < syncData.length; ii++){
+							rTrip = (syncData[ii]['t'] || '');
+							rName = (syncData[ii]['n'] || 'Anonymous');
+							tgt = $('.name', $('#pc'+syncData[ii]['p'], $('#t'+t)));
+							synced = $.htm(tgt, rName + ' <span style="font-weight:normal;">' + rTrip + '</span>');
+						}
+						return x.responseText;
+					}
 				}
+				x.open("GET", "https://www.namesync.org/namesync/qp.php?t="+t+"&b="+b, true);
+				x.setRequestHeader('X-Requested-With', 'NameSync4.5.2');
+				x.send();
 			}
-			x.open("GET", "https://www.namesync.org/namesync/qp.php?t="+t+"&b="+b, true);
-			x.setRequestHeader('X-Requested-With', 'NameSync4.5.2');
-			x.send();
-	}
 
-	var $threads = $$('.thread');
+        var $threads = $$('.thread');
+		function gPage(){
+			for(var n=0; n < $threads.length; n++){
+					var $board = window.location.href.split('.org/')[1].split('/')[0];        
+					var $thread = $threads[n].id.split('t')[1];        
+					$.get($thread, $board);
+			}
+		}
+		gPage();
+		
+		var $di, $ci;
+		
+		$ci = setInterval(function(){ei()},100);
+		
+		function ei(){
+			if($.htm($('.mobile-tu-status')) == 'Updating...'){
+				$di = setInterval(function(){bi()},100);
+				clearInterval($ci);
+			}
+		}
+		
+		function bi(){
+			if($.htm($('.mobile-tu-status')) != 'Updating...'){
+				gPage();
+				$ci = setInterval(function(){ei()},100);
+				clearInterval($di);
+			}
+		}
 
-	for(var n=0; n < $threads.length; n++){
-		var $board = window.location.href.split('.org/')[1].split('/')[0];	
-		var $thread = $threads[n].id.split('t')[1];	
-		$.get($thread, $board);
-	}
-
-} document.addEventListener("DOMContentLoaded", init, false);
+} document.addEventListener("DOMContentLoaded", initCutesync, false);
